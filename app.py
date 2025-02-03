65fe85b73cf382e285ee7bf5f5c88b6930fc674b
大山 友一 <oyama.contact@gmail.com>
2025-02-04 01:31:31 +0900

    ヘルスチェックのエンドポイントを追加

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/system/health")
+def health_check():
+    return jsonify({"status": "ok"}), 200
+
 @app.route("/")
 def hello():
-    return "Hello World1"
+    return "こんにちは、大山君！"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
